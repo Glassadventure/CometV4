4169f46792a86a9265a9edef397394187bb53368
Chronos <112827733+ChronoAccelerator@users.noreply.github.com>
2022-11-28 19:07:27 -0800

    Create Screech.lua

---
+++ Entities/Screech.lua
@@ -0,0 +1,2 @@
+local Data = require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech)(Data)
