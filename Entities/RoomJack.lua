1a1bd0053c319c0b2a9df8802b13440405f0e64a
Chronos <112827733+ChronoAccelerator@users.noreply.github.com>
2022-11-30 14:40:45 -0800

    Create RoomJack.lua

---
+++ Entities/RoomJack.lua
@@ -0,0 +1,12 @@
+ game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+local currentLoadedRoom=workspace.CurrentRooms[game:GetService("ReplicatedStorage").GameData.LatestRoom.Value]
+local shadow=game:GetObjects("rbxassetid://11446576619")[1]
+firesignal(game.ReplicatedStorage.Bricks.UseEventModule.OnClientEvent, "flickerLights", game.ReplicatedStorage.GameData.LatestRoom.Value, 1)
+shadow:PivotTo(currentLoadedRoom.RoomStart.CFrame)
+wait(0.2)
+shadow.Parent=workspace
+shadow.Sound:Play()
+task.wait(0.3)
+shadow:Destroy()
+firesignal(game.ReplicatedStorage.Bricks.UseEventModule.OnClientEvent, "tryp", workspace.CurrentRooms[game.ReplicatedStorage.GameData.LatestRoom.Value], 1)
+end)
