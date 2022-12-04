29af9cd3819d3c9a7664b467210ba7b032ee8fc5
Chronos <112827733+ChronoAccelerator@users.noreply.github.com>
2022-12-03 17:08:59 -0800

    Update Skeletonkey.lua

--- Items/Skeletonkey.lua
+++ Items/Skeletonkey.lua
@@ -1,68 +1,40 @@
-local Players = game:GetService("Players")
-local TS = game:GetService("TweenService")
-local ReSt = game:GetService("ReplicatedStorage")
-local PPS = game:GetService("ProximityPromptService")
-
-local Plr = Players.LocalPlayer
-local Char = Plr.Character or Plr.CharacterAdded:Wait()
-local Hum = Char:WaitForChild("Humanoid")
-
-local SelfModules = {
-    Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))(),
-    DoorReplication = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Door%20Replication/Source.lua"))(),
-    Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))(),
-    CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))(),
-}
-local Assets = {
-    KeyItem = LoadCustomInstance("https://github.com/lopvi/Doors/blob/main/Skele%20Key.rbxm?raw=true"),
-}
-
-
-local function replicateDoor(room)
-    local originalDoor = room:FindFirstChild("Door")
-
-    if originalDoor then
-        local door = SelfModules.DoorReplication.CreateDoor({
-            Locked = room:WaitForChild("Assets"):WaitForChild("KeyObtain", 0.3) ~= nil,
-            Sign = true,
-            Light = true,
-            Barricaded = false,
-            CustomKeyNames = {"Skele Key"},
-            DestroyKey = false,
-            GuidingLight = true,
-            FastOpen = false,
-        })
-
-        door.Model.Name = "FakeDoor"
-        door.Model:SetPrimaryPartCFrame(originalDoor.PrimaryPart.CFrame)
-        door.Model.Parent = room
-        SelfModules.DoorReplication.ReplicateDoor(door)
-        originalDoor:Destroy()
-
-end
-end
-
--- Door replication setup
-
-task.spawn(function()
-    for _, v in next, workspace.CurrentRooms:GetChildren() do
-        if v:FindFirstChild("Door") and v.Door:FindFirstChild("Lock") then
-            replicateDoor(v)
+if not isfile("skellyKey.rbxm") then
+            writefile("skellyKey.rbxm", game:HttpGet"https://raw.githubusercontent.com/sponguss/Doors-Entity-Replicator/main/skellyKey.rbxm")
         end
-    end
-    
-    workspace.CurrentRooms.DescendantAdded:Connect(function(des)
-        if des.Name == "Lock" and des.Parent.Name == "Door" then
-            task.wait(0.3)
+        local keyTool: Tool=game:GetObjects((getcustomasset or getsynasset)("skellyKey.rbxm"))[1]
+        keyTool:SetAttribute("uses", math.huge) --uses u want to put to the key
 
-            if des.Parent then
-                replicateDoor(des.Parent.Parent)
+        local function setupRoom(room)
+            local thing=loadstring(game:HttpGet"https://raw.githubusercontent.com/sponguss/Doors-Entity-Replicator/main/skellyKeyRoomRep.lua")()
+            local newdoor=thing.CreateDoor({CustomKeyNames={"SkellyKey"}, Sign=true, Light=true, Locked=true})
+            newdoor.Model.Parent=workspace
+            newdoor.Model:PivotTo(room.Door.Door.CFrame)
+            newdoor.Model.Parent=room
+            room.Door:Destroy()
+            thing.ReplicateDoor({Model=newdoor.Model, Config={CustomKeyNames={"SkellyKey"}}, Debug={OnDoorPreOpened=function() end}})
+        end
+        keyTool.Equipped:Connect(function()
+            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                if room.Door:FindFirstChild"Lock" and not room:GetAttribute("Replaced") then
+                    room:SetAttribute("Replaced", true)
+                    setupRoom(room)
+                end
             end
-        end
-    end)
-end)
+            con=workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                if room.Door:FindFirstChild"Lock" and not room:GetAttribute("Replaced") then
+                    room:SetAttribute("Replaced", true)
+                    setupRoom(room)
+                end
+            end)
+        end)
+        keyTool.Unequipped:Connect(function() con:Disconnect() end)
 
--- Obtain cursed key
-
-Assets.KeyItem.Parent = game.Players.LocalPlayer.Backpack
- firesignal(ReSt.Bricks.Caption.OnClientEvent, "You got Skeleton Key!", true)
+        if game.Players.LocalPlayer.PlayerGui.MainUI.ItemShop.Visible then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))().CreateItem(keyTool, {
+                Title = "Skeleton Key",
+                Desc = "holds secrets",
+                Image = "https://static.wikia.nocookie.net/doors-game/images/8/88/Icon_crucifix2.png/revision/latest/scale-to-width-down/350?cb=20220728033038",
+                Price = 100,
+                Stack = 1,
+            })
+        else keyTool.Parent=game.Players.LocalPlayer.Backpack end
